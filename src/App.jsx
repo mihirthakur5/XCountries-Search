a1fc3cfb4d7d1e932732411640341e94433b9c3f
Mihir Thakur <mihirthakur5@gmail.com>
2024-03-28 19:44:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import styles from "./App.module.css";
+
+function App() {
+  const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const getCountriesData = async () => {
+    try {
+      const data = await fetch("https://restcountries.com/v3.1/all");
+      const res = await data.json();
+      setCountries(res);
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
+  };
+
+  const filteredCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(search.toLowerCase())
+  );
+
+  useEffect(() => {
+    getCountriesData();
+  }, []);
+
+  return (
+    <>
+      <div className={styles.searchBar}>
+        <input
+          type="text"
+          placeholder="Search for countries..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      <div className={styles.container}>
+        {filteredCountries.map((country) => {
+          return (
+            <div key={country.cca3} className={styles.countryCard}>
+              <img
+                src={country.flags.png}
+                alt={`Flag of ${country.name.common}`}
+                className={styles.imageStyle}
+              />
+              <p className={styles.textStyle}>{country.name.common}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
